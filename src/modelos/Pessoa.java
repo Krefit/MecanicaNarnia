d20523158172a85d2f9c51024362a5a030e82120
tanakadanilo <tanakadanilo867@gmail.com>
2022-03-02 16:12:36 -0300

    criando superclasse pessoa

---
+++ src/modelos/Pessoa.java
@@ -0,0 +1,10 @@
+// Essa classe será usada como a superclasse de todas as classes que tenham pessoas, pois teram vários atributos iguais
+package modelos;
+
+/**
+ *
+ * @author tanak
+ */
+public class Pessoa {
+
+}
