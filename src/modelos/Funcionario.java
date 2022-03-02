7aa27d0af687408d2952aef8a61a55ad46052c67
tanakadanilo <tanakadanilo867@gmail.com>
2022-03-02 16:19:32 -0300

    modificando para ficar de acordo com a superclasse pessoa

--- src/modelos/Funcionario.java
+++ src/modelos/Funcionario.java
@@ -10,89 +10,23 @@ import java.util.ArrayList;
  *
  * @author ALUNO
  */
-public class Funcionario {
-
-    private String nome = "";
-    private String cpf = "";
-    private String endereco = "";
-    private String telefone = "";
+public class Funcionario extends Pessoa {
 
 // * O campo telefone secundáriio é o único que não é obrigatório ser preenchido quando se adiciona um novo fornecedor
     private String telefoneSecundario = "";
-    private String email = "";
 
 // *A variável produtos, armazena os nomes e os respectivos preços de cada produto fornecido por esse fornecedor     
     private ArrayList<String> produtos[][];
 
     public Funcionario(String nome, String cpf, String endereco, String telefone, String email) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.endereco = endereco;
-        this.telefone = telefone;
-        this.email = email;
+
+        super(nome, cpf, telefone, email, endereco);
+
     }
 
     public Funcionario(String nome, String cpf, String endereco, String telefone, String telefoneSecundario, String email) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.endereco = endereco;
-        this.telefone = telefone;
+        super(nome, cpf, telefone, email, endereco);
         this.telefoneSecundario = telefoneSecundario;
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) throws Exception{
-        if(validaCPF(cpf)){
-        this.cpf = cpf;
-        }
-        else{
-            throw new Exception("CPF inválido");
-        }
-    }
-    
-
-    public String getEndereço() {
-        return endereco;
-    }
-
-    public void setEndereço(String endereço) {
-        this.endereco = endereço;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getTelefoneSecundario() {
-        return telefoneSecundario;
-    }
-
-    public void setTelefoneSecundario(String telefoneSecundario) {
-        this.telefoneSecundario = telefoneSecundario;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public ArrayList<String>[][] getProdutos() {
@@ -102,56 +36,5 @@ public class Funcionario {
     public void setProdutos(ArrayList<String>[][] produtos) {
         this.produtos = produtos;
     }
-    
-     private boolean validaCPF(String CPF) {
-        if (CPF.equals("00000000000")
-                || CPF.equals("11111111111")
-                || CPF.equals("22222222222") || CPF.equals("33333333333")
-                || CPF.equals("44444444444") || CPF.equals("55555555555")
-                || CPF.equals("66666666666") || CPF.equals("77777777777")
-                || CPF.equals("88888888888") || CPF.equals("99999999999")
-                || (CPF.length() != 11)) {
-            return (false);
-        }
 
-        char dig10, dig11;
-        int sm, i, r, num, peso;
-
-        sm = 0;
-        peso = 10;
-        for (i = 0; i < 9; i++) {
-            num = (int) (CPF.charAt(i) - 48);
-            sm = sm + (num * peso);
-            peso = peso - 1;
-        }
-
-        r = 11 - (sm % 11);
-        if ((r == 10) || (r == 11)) {
-            dig10 = '0';
-        } else {
-            dig10 = (char) (r + 48);
-        }
-
-        sm = 0;
-        peso = 11;
-        for (i = 0; i < 10; i++) {
-            num = (int) (CPF.charAt(i) - 48);
-            sm = sm + (num * peso);
-            peso = peso - 1;
-        }
-
-        r = 11 - (sm % 11);
-        if ((r == 10) || (r == 11)) {
-            dig11 = '0';
-        } else {
-            dig11 = (char) (r + 48);
-        }
-
-        if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10))) {
-            return (true);
-        } else {
-            return (false);
-        }
-
-    }
 }
