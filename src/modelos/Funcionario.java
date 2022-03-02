663f6bb4521d1c8b4505ab6550a58f66ebe6b34a
tanakadanilo <tanakadanilo867@gmail.com>
2022-03-02 15:48:52 -0300

    criando atributo cpf e validando cpf

--- src/modelos/Funcionario.java
+++ src/modelos/Funcionario.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 public class Funcionario {
 
     private String nome = "";
+    private String cpf = "";
     private String endereco = "";
     private String telefone = "";
 
@@ -23,15 +24,17 @@ public class Funcionario {
 // *A variável produtos, armazena os nomes e os respectivos preços de cada produto fornecido por esse fornecedor     
     private ArrayList<String> produtos[][];
 
-    public Funcionario(String nome, String endereco, String telefone, String email) {
+    public Funcionario(String nome, String cpf, String endereco, String telefone, String email) {
         this.nome = nome;
+        this.cpf = cpf;
         this.endereco = endereco;
         this.telefone = telefone;
         this.email = email;
     }
 
-    public Funcionario(String nome, String endereco, String telefone, String telefoneSecundario, String email) {
+    public Funcionario(String nome, String cpf, String endereco, String telefone, String telefoneSecundario, String email) {
         this.nome = nome;
+        this.cpf = cpf;
         this.endereco = endereco;
         this.telefone = telefone;
         this.telefoneSecundario = telefoneSecundario;
@@ -45,6 +48,20 @@ public class Funcionario {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) throws Exception{
+        if(validaCPF(cpf)){
+        this.cpf = cpf;
+        }
+        else{
+            throw new Exception("CPF inválido");
+        }
+    }
+    
 
     public String getEndereço() {
         return endereco;
@@ -85,5 +102,56 @@ public class Funcionario {
     public void setProdutos(ArrayList<String>[][] produtos) {
         this.produtos = produtos;
     }
+    
+     private boolean validaCPF(String CPF) {
+        if (CPF.equals("00000000000")
+                || CPF.equals("11111111111")
+                || CPF.equals("22222222222") || CPF.equals("33333333333")
+                || CPF.equals("44444444444") || CPF.equals("55555555555")
+                || CPF.equals("66666666666") || CPF.equals("77777777777")
+                || CPF.equals("88888888888") || CPF.equals("99999999999")
+                || (CPF.length() != 11)) {
+            return (false);
+        }
 
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        sm = 0;
+        peso = 10;
+        for (i = 0; i < 9; i++) {
+            num = (int) (CPF.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+        }
+
+        r = 11 - (sm % 11);
+        if ((r == 10) || (r == 11)) {
+            dig10 = '0';
+        } else {
+            dig10 = (char) (r + 48);
+        }
+
+        sm = 0;
+        peso = 11;
+        for (i = 0; i < 10; i++) {
+            num = (int) (CPF.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+        }
+
+        r = 11 - (sm % 11);
+        if ((r == 10) || (r == 11)) {
+            dig11 = '0';
+        } else {
+            dig11 = (char) (r + 48);
+        }
+
+        if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10))) {
+            return (true);
+        } else {
+            return (false);
+        }
+
+    }
 }
