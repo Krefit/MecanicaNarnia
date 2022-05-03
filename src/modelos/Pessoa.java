9bd208448c80be214616c9363032f31956ea5c1d
Danilo Tanaka <tanakadanilo867@gmail.com>
2022-05-03 15:11:59 -0300

    correcao - corrigindo validacao de telefone

--- src/modelos/Pessoa.java
+++ src/modelos/Pessoa.java
@@ -59,12 +59,12 @@ public class Pessoa {
     }
 
     public boolean validaTelefone(String telefoneParaValidar) {
-        formataTelefone(telefoneParaValidar);//formatando o telefone para colocar os parenteses no DDD e o hifen no meio do numero
+        telefoneParaValidar = formataTelefone(telefoneParaValidar);//formatando o telefone para colocar os parenteses no DDD e o hifen no meio do numero
         return (telefoneParaValidar.matches("^\\([1-9]{2}\\) 9[7-9]{1}[0-9]{3}\\-[0-9]{4}$"));
         // o método matches, diz se uma String segue o padrão informado, que no caso é o padrão de telefone, com parênteses e o hifen "(xx) 9XXXX-XXXX"
     }
 
-    private String formataTelefone(String telefoneParaFormatar) {
+    public String formataTelefone(String telefoneParaFormatar) {
         if (telefoneParaFormatar.length() < 7) {//se for menor q 7 não dá pra formatar
             return "Telefone inválido";
         }
