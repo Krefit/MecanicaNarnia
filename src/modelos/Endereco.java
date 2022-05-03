3aa1af7645254b1524fd812a6cc58fdc817567a4
Danilo Tanaka <tanakadanilo867@gmail.com>
2022-05-03 10:22:24 -0300

    Criando classe Endereco usando enum de estados

---
+++ src/modelos/Endereco.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import enumerations.EstadosBrazil;
+
+/**
+ *
+ * @author tanak
+ */
+public class Endereco {
+
+    private String tipoLogradouro;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private EstadosBrazil estado;
+    private String CEP;
+
+    public Endereco() {
+    }
+
+    public Endereco(String tipoLogradouro, String logradouro, String numero, String complemento, String bairro, String cidade, EstadosBrazil estado, String CEP) {
+        this.tipoLogradouro = tipoLogradouro;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.CEP = CEP;
+    }
+
+    public String getTipoLogradouro() {
+        return tipoLogradouro;
+    }
+
+    public void setTipoLogradouro(String tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public EstadosBrazil getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadosBrazil estado) {
+        this.estado = estado;
+    }
+
+    public String getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    @Override
+    public String toString() {
+        return tipoLogradouro + "," + logradouro + "," + numero + "," + complemento + "," + bairro + "," + cidade + "," + estado + "," + CEP + ";";
+    }
+
+}
