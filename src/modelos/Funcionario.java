9d9d38d891026ebe357322a8cf869d4c5aba142a
Danilo Tanaka <tanakadanilo867@gmail.com>
2022-05-13 15:54:21 -0300

    criando classe funcionario

--- src/modelos/Funcionario.java
+++ src/modelos/Funcionario.java
@@ -6,33 +6,24 @@ package modelos;
 
 import modelos.auxiliares.Endereco;
 import java.util.Date;
+
 /**
  *
  * @author ALUNO
  */
 public class Funcionario extends PessoaFisica {
 
-// * O campo telefone secundáriio é o único que não é obrigatório ser preenchido quando se adiciona um novo fornecedor
-    private String telefoneSecundario = "";
-    private String especialidade = "";
+    private String especialidade;
+    private double salarioMensal;
+    private double salariohora;
+    private int matricula;
 
-    public Funcionario(String nome, String cpf, Date dataNascimento, String telefone, String email, Endereco endereco, String especialidade) {
+    public Funcionario(String especialidade, double salarioMensal, double salariohora, int matricula, String nome, String cpf, Date dataNascimento, String telefone, String email, Endereco endereco) {
         super(nome, cpf, dataNascimento, telefone, email, endereco);
         this.especialidade = especialidade;
-    }
-
-    public Funcionario(String nome, String cpf, Date dataNascimento, String telefone, String telefoneSecundario, String email, Endereco endereco, String especialidade) {
-        super(nome, cpf, dataNascimento, telefone, email, endereco);
-        this.telefoneSecundario = telefoneSecundario;
-        this.especialidade = especialidade;
-    }
-
-    public String getTelefoneSecundario() {
-        return telefoneSecundario;
-    }
-
-    public void setTelefoneSecundario(String telefoneSecundario) {
-        this.telefoneSecundario = telefoneSecundario;
+        this.salarioMensal = salarioMensal;
+        this.salariohora = salariohora;
+        this.matricula = matricula;
     }
 
     public String getEspecialidade() {
@@ -43,9 +34,33 @@ public class Funcionario extends PessoaFisica {
         this.especialidade = especialidade;
     }
 
+    public double getSalarioMensal() {
+        return salarioMensal;
+    }
+
+    public void setSalarioMensal(double salarioMensal) {
+        this.salarioMensal = salarioMensal;
+    }
+
+    public double getSalariohora() {
+        return salariohora;
+    }
+
+    public void setSalariohora(double salariohora) {
+        this.salariohora = salariohora;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
     @Override
     public String toString() {
-        return super.toString() + ";" + telefoneSecundario + ";" + especialidade;
+        return super.toString() + ";" + especialidade + ";" + salarioMensal + ";" + salariohora + ";" + matricula;
     }
 
 }
