b8b539e8aa74fa1eb779311d2dee139d5db1726e
Danilo Tanaka <tanakadanilo867@gmail.com>
2022-05-20 21:45:00 -0300

    Criando classe para armazenar os dados dos servicos oferecidos pela mecanica

---
+++ src/modelos/Servico.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author tanak
+ */
+public class Servico {
+
+    private final static String nomeArquivoDisco = "clientePessoaFisica.txt";
+    private final static String arquivoID = "idGeradoClientePessoaFisica.txt";
+
+    private String nomeServico;
+    private double valorMaoDeObra;
+
+    public Servico() {
+    }
+
+    public Servico(String nomeServico, double valorMaoDeObra) {
+        this.nomeServico = nomeServico;
+        this.valorMaoDeObra = valorMaoDeObra;
+    }
+
+    public static String getNomeArquivoDisco() {
+        return nomeArquivoDisco;
+    }
+
+    public static String getArquivoID() {
+        return arquivoID;
+    }
+
+    public String getNomeServico() {
+        return nomeServico;
+    }
+
+    public void setNomeServico(String nomeServico) {
+        this.nomeServico = nomeServico;
+    }
+
+    public double getValorMaoDeObra() {
+        return valorMaoDeObra;
+    }
+
+    public void setValorMaoDeObra(double valorMaoDeObra) {
+        this.valorMaoDeObra = valorMaoDeObra;
+    }
+
+    @Override
+    public String toString() {
+        return nomeServico + ";" + valorMaoDeObra;
+    }
+
+}
