782843f2c6f750d24070da76b5efbc63e8a0c2b7
Danilo Tanaka <tanakadanilo867@gmail.com>
2022-05-22 15:07:10 -0300

    correcao - correcao no alinhamento da tabela

--- src/tela/TelaCadastroDeFuncionario.java
+++ src/tela/TelaCadastroDeFuncionario.java
@@ -108,7 +108,7 @@ public class TelaCadastroDeFuncionario extends javax.swing.JFrame {
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class, java.lang.String.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
                 false, false, false
