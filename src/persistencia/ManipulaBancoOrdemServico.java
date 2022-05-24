e1178ebe13da584cb026be47716cfbc2aca5e7c3
tanakadanilo <tanakadanilo867@gmail.com>
2022-05-23 22:23:35 -0300

    criando tela de busca de OSs (parcial)

--- src/persistencia/ManipulaBancoOrdemServico.java
+++ src/persistencia/ManipulaBancoOrdemServico.java
@@ -22,14 +22,14 @@ public class ManipulaBancoOrdemServico implements IManipulaBanco<OrdemDeServico>
 
     @Override
     public void incluir(OrdemDeServico obj) throws Exception {
-        try ( BufferedWriter bw = new BufferedWriter(new FileWriter(OrdemDeServico.getNomeArquivoDisco(), true))) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(OrdemDeServico.getNomeArquivoDisco(), true))) {
             bw.write(GeradorId.getID(OrdemDeServico.getArquivoID()) + ";" + obj.toString() + "\n");
         }
     }
 
     @Override
     public int buscar(OrdemDeServico obj) throws Exception {
-        try ( BufferedReader br = new BufferedReader(new FileReader(OrdemDeServico.getNomeArquivoDisco()))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(OrdemDeServico.getNomeArquivoDisco()))) {
             String linha = br.readLine();
             while (linha != null) {
                 if (linha.endsWith(obj.toString())) {//não usar o ID na comparacao, pq esse é um atributo do banco e não da classe
@@ -48,7 +48,7 @@ public class ManipulaBancoOrdemServico implements IManipulaBanco<OrdemDeServico>
 
     @Override
     public OrdemDeServico buscar(int id) throws Exception {
-        try ( BufferedReader br = new BufferedReader(new FileReader(OrdemDeServico.getNomeArquivoDisco()))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(OrdemDeServico.getNomeArquivoDisco()))) {
             String linha = br.readLine();
             while (linha != null) {
                 if (linha.startsWith(String.valueOf(id))) {//não usar o ID na comparacao, pq esse é um atributo do banco e não da classe
@@ -76,7 +76,7 @@ public class ManipulaBancoOrdemServico implements IManipulaBanco<OrdemDeServico>
     @Override
     public ArrayList<OrdemDeServico> buscarTodos() throws Exception {
         ArrayList<OrdemDeServico> listaOSs = new ArrayList<>();
-        try ( BufferedReader br = new BufferedReader(new FileReader(OrdemDeServico.getNomeArquivoDisco()))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(OrdemDeServico.getNomeArquivoDisco()))) {
             String linha = br.readLine();
             while (linha != null) {
                 String[] dados = linha.split(";");
@@ -94,9 +94,38 @@ public class ManipulaBancoOrdemServico implements IManipulaBanco<OrdemDeServico>
 
                 listaOSs.add(new OrdemDeServico(dados[1], Integer.parseInt(dados[2]), Double.parseDouble(dados[3]), DataAbertura, Integer.parseInt(dados[7]),
                         Integer.parseInt(dados[8]), Integer.parseInt(dados[9]), Double.parseDouble(dados[10]), Integer.parseInt(dados[11])));
+                linha = br.readLine();
             }
         }
-        return null;
+        return listaOSs;
+    }
+
+    public ArrayList<OrdemDeServico> buscarTodos(int idVeiculo) throws Exception {
+        ArrayList<OrdemDeServico> listaOSs = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(OrdemDeServico.getNomeArquivoDisco()))) {
+            String linha = br.readLine();
+            while (linha != null) {
+                String[] dados = linha.split(";");
+                if (dados[11].equals(String.valueOf(idVeiculo))) {
+//  * id, defeito relatado, servico feito, Valor mao de obra, data de criacao da OS (dd/MM/yyyy), data de finalizacao da OS(dd/MM/yyyy), 
+//  * situacao da OS, id do funcionario responsável, iD da peca usada, quantidade de pecas usadas, valor unitario da peca, id do veiculo
+                    if (dados.length != 12) {
+                        //linha com defeito
+                    }
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                    Date DataAbertura = sdf.parse(dados[4]);
+                    Date dataFechamento = null;
+                    if (!dados[5].equals("null")) {//não tentar se o valor não for uma data
+                        dataFechamento = sdf.parse(dados[5]);
+                    }
+
+                    listaOSs.add(new OrdemDeServico(dados[1], Integer.parseInt(dados[2]), Double.parseDouble(dados[3]), DataAbertura, Integer.parseInt(dados[7]),
+                            Integer.parseInt(dados[8]), Integer.parseInt(dados[9]), Double.parseDouble(dados[10]), Integer.parseInt(dados[11])));
+                }
+                linha = br.readLine();
+            }
+        }
+        return listaOSs;
     }
 
     @Override
