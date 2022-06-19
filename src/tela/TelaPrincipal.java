3435ebc9aecbfd50379c2c9d01a0a5af86fa6ad9
Danilo Tanaka <tanakadanilo867@gmail.com>
2022-06-19 15:37:13 -0300

    Revert "Merge branch 'main' of https://github.com/Krefit/MecanicaNarnia"
    This reverts commit 305c00fa531f011c79707fc97ff611c062b6539c, reversing
    changes made to c406e0a230e527746d91e624eac20a9cca41e29e.

--- src/tela/TelaPrincipal.java
+++ src/tela/TelaPrincipal.java
@@ -60,7 +60,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
         jMenuCliente = new javax.swing.JMenu();
         jMenuItemClientes = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
-        jMenuItemCadastroDeOS = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenu4 = new javax.swing.JMenu();
 
@@ -111,17 +110,9 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
         jMenuBar1.add(jMenuCliente);
 
-        jMenu3.setText("Ordens de Serviço");
+        jMenu3.setText("Produtos");
 
-        jMenuItemCadastroDeOS.setText("Cadastro de Ordens de Serviço");
-        jMenuItemCadastroDeOS.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItemCadastroDeOSActionPerformed(evt);
-            }
-        });
-        jMenu3.add(jMenuItemCadastroDeOS);
-
-        jMenuItem2.setText("Ordens de Serviço");
+        jMenuItem2.setText("jMenuItem2");
         jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem2ActionPerformed(evt);
@@ -188,14 +179,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
         jDesktopPanePrincipal.setVisible(true);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
-    private void jMenuItemCadastroDeOSActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemCadastroDeOSActionPerformed
-                limpaTela();
-        TelaCadastroDeOS newTelaCadastroDeOS = new TelaCadastroDeOS();
-        newTelaCadastroDeOS.setVisible(true);
-        jDesktopPanePrincipal.add(newTelaCadastroDeOS);
-        jDesktopPanePrincipal.setVisible(true);
-    }//GEN-LAST:event_jMenuItemCadastroDeOSActionPerformed
-
     /**
      * @param args the command line arguments
      */
@@ -239,7 +222,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenu jMenuCliente;
     private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItemCadastroDeOS;
     private javax.swing.JMenuItem jMenuItemClientes;
     private javax.swing.JMenuItem jMenuItemSair;
     // End of variables declaration//GEN-END:variables
